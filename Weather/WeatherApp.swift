48494b1e8828b171f02dab1e1f75dfd8e44e5081
Jefferson Naranjo <jefferj080@gmail.com>
2022-04-14 15:21:29 -0500

    Se agrega el proyecto

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Apple on 21/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
